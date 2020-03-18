7c1b63d9908dfa4446eddba5d41c9b11a3305ee8
simontuo <simontuo666@gmail.com>
2020-03-18 18:06:01 +0800

    design some page

---
+++ src/components/Pagination/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="currentPage"
+        :page-sizes="[100, 200, 300, 400]"
+        :page-size="100"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="400"
+        background
+    ></el-pagination>
+</template>
+
+<script>
+export default {
+    name: "pagination",
+    methods: {
+        handleSizeChange(val) {
+            console.log(`每页 ${val} 条`);
+        },
+        handleCurrentChange(val) {
+            console.log(`当前页: ${val}`);
+        }
+    },
+    data() {
+        return {
+            currentPage: 1
+        };
+    }
+};
+</script>
--- src/views/promote/index.vue
+++ src/views/promote/index.vue
@@ -1,89 +1,85 @@
 <template>
     <div class="app-container">
-        <el-input v-model="filterText" placeholder="Filter keyword" style="margin-bottom:30px;" />
-
-        <el-tree
-            ref="tree2"
-            :data="data2"
-            :props="defaultProps"
-            :filter-node-method="filterNode"
-            class="filter-tree"
-            default-expand-all
-        />
+        <el-card shadow="never"></el-card>
+        <el-card shadow="never" class="mt-2">
+            <div class="header">
+                <span>卡片名称</span>
+                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            </div>
+            <el-table
+                class="mt-2"
+                v-loading="listLoading"
+                :data="list"
+                element-loading-text="Loading"
+                fit
+                highlight-current-row
+                header-cell-class-name="table-header"
+            >
+                <el-table-column align="center" label="ID" width="95">
+                    <template slot-scope="scope">{{ scope.$index }}</template>
+                </el-table-column>
+                <el-table-column label="Title">
+                    <template slot-scope="scope">{{ scope.row.title }}</template>
+                </el-table-column>
+                <el-table-column label="Author" width="110" align="center">
+                    <template slot-scope="scope">
+                        <span>{{ scope.row.author }}</span>
+                    </template>
+                </el-table-column>
+                <el-table-column label="Pageviews" width="110" align="center">
+                    <template slot-scope="scope">{{ scope.row.pageviews }}</template>
+                </el-table-column>
+                <el-table-column class-name="status-col" label="Status" width="110" align="center">
+                    <template slot-scope="scope">
+                        <el-tag :type="scope.row.status | statusFilter">{{ scope.row.status }}</el-tag>
+                    </template>
+                </el-table-column>
+                <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+                    <template slot-scope="scope">
+                        <i class="el-icon-time" />
+                        <span>{{ scope.row.display_time }}</span>
+                    </template>
+                </el-table-column>
+            </el-table>
+            <pagination class="mt-2" />
+        </el-card>
     </div>
 </template>
 
 <script>
+import { getList } from "@/api/table";
+import Pagination from "@/components/Pagination";
+
 export default {
-    data() {
-        return {
-            filterText: "",
-            data2: [
-                {
-                    id: 1,
-                    label: "Level one 1",
-                    children: [
-                        {
-                            id: 4,
-                            label: "Level two 1-1",
-                            children: [
-                                {
-                                    id: 9,
-                                    label: "Level three 1-1-1"
-                                },
-                                {
-                                    id: 10,
-                                    label: "Level three 1-1-2"
-                                }
-                            ]
-                        }
-                    ]
-                },
-                {
-                    id: 2,
-                    label: "Level one 2",
-                    children: [
-                        {
-                            id: 5,
-                            label: "Level two 2-1"
-                        },
-                        {
-                            id: 6,
-                            label: "Level two 2-2"
-                        }
-                    ]
-                },
-                {
-                    id: 3,
-                    label: "Level one 3",
-                    children: [
-                        {
-                            id: 7,
-                            label: "Level two 3-1"
-                        },
-                        {
-                            id: 8,
-                            label: "Level two 3-2"
-                        }
-                    ]
-                }
-            ],
-            defaultProps: {
-                children: "children",
-                label: "label"
-            }
-        };
+    components: {
+        Pagination
     },
-    watch: {
-        filterText(val) {
-            this.$refs.tree2.filter(val);
+    filters: {
+        statusFilter(status) {
+            const statusMap = {
+                published: "success",
+                draft: "gray",
+                deleted: "danger"
+            };
+            return statusMap[status];
         }
     },
-
+    data() {
+        return {
+            list: null,
+            listLoading: true
+        };
+    },
+    created() {
+        this.fetchData();
+    },
     methods: {
-        filterNode(value, data) {
-            if (!value) return true;
-            return data.label.indexOf(value) !== -1;
+        fetchData() {
+            this.listLoading = true;
+            getList().then(response => {
+                this.list = response.data.items;
+                this.listLoading = false;
+            });
         }
     }
 };
