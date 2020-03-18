3f1521f08d3fc92bc8d75fd260a28c042cb33e96
simontuo <simontuo666@gmail.com>
2020-03-18 15:58:08 +0800

    fist commit

---
+++ src/settings.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+	title: "Maguas Coupon",
+
+	logo: "https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png",
+
+	/**
+	 * @type {boolean} true | false
+	 * @description Whether fix the header
+	 */
+	fixedHeader: true,
+
+	/**
+	 * @type {boolean} true | false
+	 * @description Whether show the logo in sidebar
+	 */
+	sidebarLogo: true,
+
+	/** 
+	 * @type {boolean}
+	 * @description Whether show the logo in setting
+	 */
+	showSettings: false,
+
+	/** 
+	 * @type {boolean} 
+	 * @description Whether open the sidebar
+	*/
+	openSidebar: false
+}
