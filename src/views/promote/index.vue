f8fbdc53d52c3a1d73d57ccbb1573c2b6bc3c3c1
simontuo <simontuo666@gmail.com>
2020-03-19 11:21:04 +0800

    edit some page

--- src/views/promote/index.vue
+++ src/views/promote/index.vue
@@ -1,13 +1,60 @@
 <template>
     <div class="app-container">
-        <el-card shadow="never"></el-card>
+        <div class="card-search">
+            <el-card shadow="never">
+                <el-form :inline="true" :model="formInline" class="demo-form-inline" size="small">
+                    <el-form-item label="审批人">
+                        <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+                    </el-form-item>
+                    <el-form-item label="活动区域">
+                        <el-select v-model="formInline.region" placeholder="活动区域">
+                            <el-option label="区域一" value="shanghai"></el-option>
+                            <el-option label="区域二" value="beijing"></el-option>
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="onSubmit">查询</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-card>
+        </div>
+
         <el-card shadow="never" class="mt-2">
-            <div class="header">
-                <span>卡片名称</span>
-                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+            <div class="table-operate-bar clearfix">
+                <span class="operate-title">推广数据</span>
+                <div class="operate-button-bar">
+                    <el-button size="small" type="primary">操作按钮</el-button>
+                    <el-divider direction="vertical"></el-divider>
+                    <el-tooltip content="全屏" placement="top">
+                        <el-button type="text" class="operate-button">
+                            <svg-icon icon-class="fullScreen" />
+                        </el-button>
+                    </el-tooltip>
+                    <el-tooltip content="密度" placement="top">
+                        <el-button type="text" class="operate-button">
+                            <svg-icon icon-class="column-height" />
+                        </el-button>
+                    </el-tooltip>
+                    <el-tooltip content="刷新" placement="top">
+                        <el-button type="text" class="operate-button">
+                            <svg-icon icon-class="reload" />
+                        </el-button>
+                    </el-tooltip>
+                    <el-tooltip content="列设置" placement="top">
+                        <el-button type="text" class="operate-button">
+                            <svg-icon icon-class="seetings" />
+                        </el-button>
+                    </el-tooltip>
+                </div>
+            </div>
+            <div class="table-selected-bar clearfix">
+                <span class="selected-title">
+                    <svg-icon icon-class="info" class="title-icon" />推广数据
+                </span>
+                <el-button type="text" class="selected-button">清除</el-button>
             </div>
             <el-table
-                class="mt-2"
+                class="mt-1"
                 v-loading="listLoading"
                 :data="list"
                 element-loading-text="Loading"
@@ -15,6 +62,7 @@
                 highlight-current-row
                 header-cell-class-name="table-header"
             >
+                <el-table-column align="center" type="selection" width="95"></el-table-column>
                 <el-table-column align="center" label="ID" width="95">
                     <template slot-scope="scope">{{ scope.$index }}</template>
                 </el-table-column>
@@ -41,7 +89,7 @@
                     </template>
                 </el-table-column>
             </el-table>
-            <pagination class="mt-2" />
+            <pagination class="pagination" />
         </el-card>
     </div>
 </template>
@@ -51,6 +99,16 @@ import { getList } from "@/api/table";
 import Pagination from "@/components/Pagination";
 
 export default {
+    data() {
+        return {
+            list: null,
+            listLoading: true,
+            formInline: {
+                user: "",
+                region: ""
+            }
+        };
+    },
     components: {
         Pagination
     },
@@ -64,12 +122,6 @@ export default {
             return statusMap[status];
         }
     },
-    data() {
-        return {
-            list: null,
-            listLoading: true
-        };
-    },
     created() {
         this.fetchData();
     },
@@ -80,8 +132,69 @@ export default {
                 this.list = response.data.items;
                 this.listLoading = false;
             });
+        },
+        onSubmit() {
+            console.log("submit!");
         }
     }
 };
 </script>
+
+<style lang="scss" scoped>
+.card-search {
+    .el-card >>> .el-card__body {
+        padding: 20px 20px 0 20px;
+    }
+}
+.table-operate-bar {
+    margin: 0 20px;
+    line-height: 45px;
+    .operate-title {
+        font-size: 16px;
+        color: #929292;
+    }
+    .operate-button-bar {
+        float: right;
+        .operate-button {
+            font-size: 16px;
+            color: #929292;
+        }
+    }
+}
+.table-selected-bar {
+    margin: 5px 20px;
+    line-height: 40px;
+    background-color: #e6f7ff;
+    border: 1px solid #91d5ff;
+    padding: 0 20px;
+    border-radius: 2px;
+    .selected-title {
+        font-size: 14px;
+        color: #929292;
+        .title-icon {
+            color: rgb(24, 144, 255);
+            margin-right: 10px;
+        }
+    }
+    .selected-button {
+        float: right;
+        font-size: 14px;
+        color: #929292;
+    }
+}
+.el-card >>> .el-card__body {
+    padding: 10px 0;
+}
+.pagination {
+    margin: 20px 20px 0 20px;
+}
+.clearfix:after {
+    content: ""; /*设置内容为空*/
+    height: 0; /*高度为0*/
+    line-height: 0; /*行高为0*/
+    display: block; /*将文本转为块级元素*/
+    visibility: hidden; /*将元素隐藏*/
+    clear: both; /*清除浮动*/
+}
+</style>
 
